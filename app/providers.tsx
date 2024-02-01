5a12d8bc560dc91b061c311f9d0944199b3b6b92
Jeffkw213 <jeffkwan10@gmail.com>
2024-02-01 16:07:41 -0500

    idk what this one is

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,7 @@ import { NextUIProvider } from '@nextui-org/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <NextUIProvider className='h-screen grid place-items-center'>
+        <NextUIProvider className="h-screen grid place-items-center">
             {children}
         </NextUIProvider>
     )
